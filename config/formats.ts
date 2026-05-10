7a16b1a7866b8cac9bffdb349ec47919c48742fd
Max MacNeill <d3n2c@unb.ca>
2026-05-10 20:48:20 -0300

    reduce length of battle factory name

--- config/formats.ts
+++ config/formats.ts
@@ -43,7 +43,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		ruleset: ['Standard AG', 'NatDex Mod', 'Max Team Size = 24'],
 	},
 	{
-		name: "[Gen 9 Champions] Campaign Battle Factory NatDex AG",
+		name: "[Gen 9 Champions] Campaign Battle Factory",
 		desc: `Randomized teams of Pok&eacute;mon for a generated Smogon tier with sets that are competitively viable.`,
 		mod: 'champions',
 		team: 'randomFactory',
